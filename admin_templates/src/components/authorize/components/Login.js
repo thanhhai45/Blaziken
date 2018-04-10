f9e4d6189f8b914c1b38685dcbc186291238e20b
thanhnghia20495 <nghiapham20495@gmail.com>
2018-04-10 16:46:17 +0700

    display error message on login screen when user admin login failed

--- admin_templates/src/components/authorize/components/Login.js
+++ admin_templates/src/components/authorize/components/Login.js
@@ -3,26 +3,37 @@ import 'antd/dist/antd.css';
 import logo_login from './../../../images/logo-login.png'
 import { Form, Icon, Input, Button, Checkbox, Row, Col } from 'antd';
 import { Redirect, Route } from "react-router-dom";
-import App from'./../../../App'
+import App from './../../../App'
 const FormItem = Form.Item;
 
 class Login extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      message: '',
+      status:false
+    }
   }
   handleSubmit = (e) => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        localStorage.setItem('user', JSON.stringify({ values }));
+        // localStorage.setItem('user', JSON.stringify({ values }));
         console.log('Received values of form: ', values);
+        this.setState({
+          message: 'E-mail invalid',
+          status:true
+        })
       }
     });
   }
+
+
   render() {
-    if (localStorage.getItem("user")) { 
-      return <App/>
+    if (localStorage.getItem("user")) {
+      return <App />
     }
+    console.log(this.state.message)
     const { getFieldDecorator } = this.props.form;
     return (
       <Row type="flex" justify="center" align="middle">
@@ -31,10 +42,13 @@ class Login extends Component {
             <img src={logo_login} alt="logo-login" style={{ width: '50%' }} />
             <Form onSubmit={this.handleSubmit} className="login-form">
               <FormItem>
-                {getFieldDecorator('userName', {
-                  rules: [{ required: true, message: 'Please input your username!' }],
+                {getFieldDecorator('email', {
+                  rules: [
+                    { required: true, message: 'Please input your E-email!' },
+                    { type: 'email', message: 'The input is not valid E-mail!' }
+                        ]
                 })(
-                  <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                  <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="E-mail"/>
                 )}
               </FormItem>
               <FormItem>
